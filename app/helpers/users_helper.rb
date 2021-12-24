51140cd91458a7fa101bbeafc76dd8c407ef79f4
brady <bradycar@gmail.com>
2021-12-24 12:22:38 -0300

    initial-hotwire

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+    NOTICE = {
+        create: 'User created successfully',
+        update: 'User updated successfully',
+        delete: 'User deleted successfully',
+        like: 'User has been liked',
+        error: 'Something went wrong'
+      }.freeze
+    
+      def notice_stream(message:, status:)
+        turbo_stream.replace 'notice', partial: 'notice', locals: { notice: NOTICE[message], status: status }
+      end
+    
+      def form_user_stream(user:)
+        turbo_stream.replace 'form', partial: 'form', locals: { user: user }
+      end
 end
